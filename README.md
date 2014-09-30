cf6137e24d54eee92df6c84790e513e065dc1e0a
Jude Payne <judepayne@yahoo.co.uk>
2014-09-30 22:38:47 +0100

    fixed ns issues & general tidy

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ A clojure library for performance benchmarking IO type operations. e.g. database
 
 add to your project.clj
 
-    [perf-bench "0.1.2"]
+    [perf-bench "0.1.4"]
 
 use in code
 
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject perf-bench "0.1.2"
+(defproject perf-bench/perf-bench "0.1.4"
   :description "mini IO performance benchmarking library for Clojure"
   :url "http://github.com/judepayne/perf-bench"
   :scm {:name "git"
@@ -6,4 +6,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/math.numeric-tower "0.0.4"]])
+                 [org.clojure/math.numeric-tower "0.0.4"]]
+  )
